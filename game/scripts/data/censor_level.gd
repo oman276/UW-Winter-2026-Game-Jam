238a80354244917ff7c8e4158bcd72e3be3995ed
Owen Gallagher <owengallagher276@gmail.com>
2026-01-30 18:40:52 -0500

    Added censor level w info

---
+++ game/scripts/data/censor_level.gd
@@ -0,0 +1,7 @@
+extends OwenLevel
+class_name CensorLevel
+
+@export var attributes_must_exclude : Array[Attribute] = []
+@export var attributes_must_include : Array[Attribute] = []
+
+@export var files_to_load : Array[Control] = []
