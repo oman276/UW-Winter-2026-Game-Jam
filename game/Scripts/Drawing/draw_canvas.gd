a080ecd06463faeb885ba46e981feb2af0196bd0
Saaz Ramsubhag <ramsubhag.saaz@gmail.com>
2026-01-30 22:48:51 -0400

    Added texture to the pen

--- game/Scripts/Drawing/draw_canvas.gd
+++ game/Scripts/Drawing/draw_canvas.gd
@@ -1,12 +1,17 @@
 extends Node2D
 
+@export var line_texture : Texture2D
 
 var _ink_color := Color.RED
+var _line_width := 6.0
 var _pressed := false
 var _current_line: Line2D = null
 
-func set_ink_color(color: Color):
+func set_ink_color(color: Color) -> void:
 	_ink_color = color
+	
+func set_line_width(width: float) -> void:
+	_line_width = width
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
@@ -14,12 +19,18 @@ func _input(event: InputEvent) -> void:
 
 			if !_pressed:
 				_current_line = Line2D.new()
+				_current_line.texture = line_texture
+				_current_line.texture_mode = Line2D.LINE_TEXTURE_TILE
+				_current_line.texture_repeat = CanvasItem.TEXTURE_REPEAT_ENABLED
+				_current_line.antialiased = true
+				_current_line.begin_cap_mode = Line2D.LINE_CAP_ROUND
 				_current_line.end_cap_mode = Line2D.LINE_CAP_ROUND
+				_current_line.joint_mode = Line2D.LINE_JOINT_ROUND
 				_current_line.default_color = _ink_color
-				_current_line.width = 6
+				_current_line.width = _line_width
 				add_child(_current_line)
 				_current_line.add_point(event.position)
-				_current_line.add_point(event.position)
+				#_current_line.add_point(event.position)
 				
 			_pressed = event.pressed
 		
@@ -27,7 +38,7 @@ func _input(event: InputEvent) -> void:
 			clear_canvas()
 
 	elif event is InputEventMouseMotion and _pressed:
-		if _current_line:
+		if _current_line and _current_line.get_point_position(_current_line.get_point_count()-1).distance_to(event.position) > 5:
 			_current_line.add_point(event.position)
 
 func clear_canvas():
--- game/Scripts/Drawing/draw_view_container.gd
+++ game/Scripts/Drawing/draw_view_container.gd
@@ -1,9 +1,10 @@
 extends SubViewportContainer
 
-var ink_color : Color = Color.BLACK
-
-func set_ink_color(color: Color):
-	$SubViewport/DrawCanvas.set_ink_color(ink_color)	
+func set_ink_color(color: Color) -> void:
+	$SubViewport/DrawCanvas.set_ink_color(color)
+	
+func set_line_width(width: float) -> void:
+	$SubViewport/DrawCanvas.set_line_width(width)
 	
 func get_drawing() -> Image:
 	return $SubViewport.get_texture().get_image()
